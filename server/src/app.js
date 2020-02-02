1580f2d6d9f6ed688e32eca2b14b077040f0f58c
Rhonda Kremer <rhondakremer@gmail.com>
2020-02-02 10:29:27 -0500

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// support parsing of application/json type post data
+    app.use(bodyParser.json());
+
+// support parsing of application/x-www-form-urlencoded post data
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+// support for using cors
+    app.use(cors());
+
+// HTTP request logger middleware for node.js
+    app.use(morgan('combined'))
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: "You go, server!"
+    })
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("server now listening on port 3000")
+});
