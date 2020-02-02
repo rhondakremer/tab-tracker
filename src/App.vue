19d544d83f9208f633dd0893be7e8464815c0d31
Rhonda Kremer <rhondakremer@gmail.com>
2020-02-02 09:47:19 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -9,7 +8,6 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
   components: {
     HelloWorld
   }
@@ -17,12 +15,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
